82e88106aa805507a00bd3a1eb41d5e76fe70e4b
DharmendraKumar Jagodana <jagodana.dharmendra@gmail.com>
2022-02-01 17:55:25 +0530

    Feat: add findAsPDF and SendEmail API

--- controllers/assessments.js
+++ controllers/assessments.js
@@ -1,8 +1,16 @@
+const { sendMail } = require("../helpers/sendgrid-mail");
+
 const { BTAssessments } = require("../models/bt-assessments");
 const { STAssessments } = require("../models/st-assessments");
 const { OTAssessments } = require("../models/ot-assessments");
+const { Client } = require("../models/client");
 
 const { v4: uuidv4 } = require('uuid');
+const { createPDF } = require("./createPDF");
+
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
 
 const defaultResponseObject = {
     success: true,
@@ -49,8 +57,54 @@ const create = async (req, res) => {
 }
 
 const findAsPDF = async (req, res) => {
-    console.log(req);
-    res.json({ success: true });
+    try {
+        //find client email
+        const { client_id, assessmentType } = req.query;
+        const result = await Client.find({ client_id: client_id }, "mobile_no name gender address email created_on created_by assessment chief_complaints diagnosis therapy").lean();
+        if (!result || result.length == 0) {
+            throw Error("No Client found for given client id:" + client_id);
+        }
+        const clientInfo = result[0];
+
+        const fileName = `${client_id}-${assessmentType}.pdf`;
+
+        //find assessment data
+        let assessmentResult = [];
+        let query = { client_id: client_id };
+        let filter = "";
+        switch (assessmentType) {
+            case "BT":
+                filter = "assessmentType assessment_date behavior_observation created_by created_on development_history family_history impression prenatal_history recommendations school_history test_results tests_administered therapist";
+                assessmentResult = await BTAssessments.find(query, filter).lean();
+                break;
+
+            case "ST":
+                filter = "assessment_date attention_level attention_to_sound daddling environmental_sounds expressive_lang eye_contact family_history first_word hearing initiate_interaction limitation_to_body_movements limitation_to_speech main_mode_comm motor_developments oro_peripheral_mechanism provisional_diagnosis receptive_lang recommendations reels_el_score reels_rl_score response_to_name_call social_smile test_administered therapist vegetative_skills vision";
+                assessmentResult = await STAssessments.find(query, filter).lean();
+                break;
+
+            case "OT":
+                filter = "assessment_date behavior_cognition cognitive_skills kinaesthesia milestone_development presenting_complaints therapist";
+                assessmentResult = await OTAssessments.find(query, filter).lean();
+                break;
+        }
+
+        //generate pdf;
+        createPDF("Hello", clientInfo, "Assessments", assessmentResult[0], fileName);
+        await sleep(1000);
+
+        let response = { ...defaultResponseObject };
+        response.message = "PDF fetched successfully";
+        response.data = { fileName: fileName };
+        res.status(200).send(response);
+        // res.status(200).sendFile(publicPath);
+    } catch (e) {
+        console.log(e);
+        let response = { ...defaultResponseObject };
+        response.error = e.message || e;
+        response.success = false;
+        res.status(400).send(response);
+    }
 }
 
 const findAll = async (req, res) => {
@@ -143,8 +197,58 @@ const update = async (req, res) => {
 }
 
 const email = async (req, res) => {
-    console.log(req);
-    res.json({ success: true });
+    try {
+
+        //find client email
+        const { client_id, assessmentType } = req.body;
+        const result = await Client.find({ client_id: client_id }, "mobile_no name gender address email created_on created_by assessment chief_complaints diagnosis therapy").lean();
+        if (!result || result.length == 0) {
+            throw Error("No Client found for given client id:" + client_id);
+        }
+        const clientInfo = result[0];
+        const toEmail = result[0].email;
+
+        const fileName = `${client_id}.pdf`;
+
+        //find assessment data
+        let assessmentResult = [];
+        let query = { client_id: client_id };
+        let filter = "";
+        switch (assessmentType) {
+            case "BT":
+                filter = "assessmentType assessment_date behavior_observation created_by created_on development_history family_history impression prenatal_history recommendations school_history test_results tests_administered therapist";
+                assessmentResult = await BTAssessments.find(query, filter).lean();
+                break;
+
+            case "ST":
+                filter = "assessment_date attention_level attention_to_sound daddling environmental_sounds expressive_lang eye_contact family_history first_word hearing initiate_interaction limitation_to_body_movements limitation_to_speech main_mode_comm motor_developments oro_peripheral_mechanism provisional_diagnosis receptive_lang recommendations reels_el_score reels_rl_score response_to_name_call social_smile test_administered therapist vegetative_skills vision";
+                assessmentResult = await STAssessments.find(query, filter).lean();
+                break;
+
+            case "OT":
+                filter = "assessment_date behavior_cognition cognitive_skills kinaesthesia milestone_development presenting_complaints therapist";
+                assessmentResult = await OTAssessments.find(query, filter).lean();
+                break;
+        }
+
+        //generate pdf;
+        const finalFilePath = createPDF("Hello", clientInfo, "Assessments", assessmentResult[0], fileName);
+        await sleep(1000);
+
+        // send email
+        const resultSendMail = await sendMail(toEmail, finalFilePath);
+        let response = { ...defaultResponseObject };
+        response.message = "Data fetched successfully";
+        response.data = resultSendMail.toString();
+        res.status(200).send(response);
+
+    } catch (e) {
+        console.log(e);
+        let response = { ...defaultResponseObject };
+        response.error = e.message || e;
+        response.success = false;
+        res.status(400).send(response);
+    }
 }
 
 module.exports = {
